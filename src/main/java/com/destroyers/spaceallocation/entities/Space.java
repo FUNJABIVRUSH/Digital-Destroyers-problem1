febe1a638db376f4a52c36121d37c74238fe14fc
saurabh.patel <saurabh.patel@thoughtworks.com>
2022-09-17 11:12:40 +0530

    Adds template code for space allocation

---
+++ src/main/java/com/destroyers/spaceallocation/entities/Space.java
@@ -0,0 +1,22 @@
+package com.destroyers.spaceallocation.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "space")
+@Entity
+public class Space {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
--- src/main/java/com/destroyers/spaceallocation/model/space/AllocateSpaceRequest.java
+++ src/main/java/com/destroyers/spaceallocation/model/space/AllocateSpaceRequest.java
@@ -13,8 +13,8 @@ public class AllocateSpaceRequest {
 
     private String buildingId;
     private String zoneId;
-    private String seatStart;
-    private String seatEnd;
+    private String startSeatNo;
+    private String endSeatNo;
     private String departmentId;
     private List<String> oeCodes;
 
