2bf086c6e01ebbe983bab146e8ca0312fb41a370
Shilkumar Jadhav <shilkumarjadhav@gmail.com>
2022-09-18 21:38:34 +0530

    Added employee test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -48,26 +48,26 @@ INSERT INTO seat(id, number, zone_id, type) VALUES (30, '5', 6, 'NON_WINDOW');
 INSERT INTO department (id, name, building_id) VALUES (1, 'Private Banking', 1);
 INSERT INTO department (id, name, building_id) VALUES (2, 'Investment Banking', 1);
 
-INSERT INTO oe_code (id, name, total_employees, department_id, type) VALUES (1, 'MBLD1', 38, 1, 'HIGH');
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (2, 'MBLD11', 14, 1, 'MID', 1);
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (3, 'MBLD111', 6, 1, 'MID', 2);
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (4, 'MBLD1111', 5, 1, 'LOW', 3);
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (5, 'MBLD112', 6, 1, 'MID', 2);
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (6, 'MBLD1121', 5, 1, 'LOW', 2);
+INSERT INTO oe_code (id, name, total_employees, department_id, type) VALUES (1, 'MBLD1', 68, 1, 'HIGH');
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (2, 'MBLD11', 24, 1, 'MID', 1);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (3, 'MBLD111', 16, 1, 'MID', 2);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (4, 'MBLD1111', 15, 1, 'LOW', 3);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (5, 'MBLD112', 16, 1, 'MID', 4);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (6, 'MBLD1121', 15, 1, 'LOW', 5);
 
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (7, 'MBLD12', 23, 1, 'MID', 1);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (7, 'MBLD12', 33, 1, 'MID', 1);
 INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (8, 'MBLD121', 6, 1, 'MID', 7);
 INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (9, 'MBLD1211', 5, 1, 'LOW', 8);
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (10, 'MBLD122', 8, 1, 'MID', 7);
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (11, 'MBLD1221', 7, 1, 'LOW', 10);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (10, 'MBLD122', 18, 1, 'MID', 7);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (11, 'MBLD1221', 17, 1, 'LOW', 10);
 INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (12, 'MBLD123', 8, 1, 'MID', 7);
 INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (13, 'MBLD1231', 7, 1, 'LOW', 12);
 
-INSERT INTO oe_code (id, name, total_employees, department_id, type) VALUES (14, 'IBLD1', 12, 1, 'HIGH');
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (15, 'IBLD11', 5, 1, 'MID', 14);
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (16, 'IBLD111', 4, 1, 'LOW', 15);
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (17, 'IBLD12', 6, 1, 'MID', 14);
-INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (18, 'IBLD121', 5, 1, 'LOW', 17);
+INSERT INTO oe_code (id, name, total_employees, department_id, type) VALUES (14, 'IBLD1', 32, 1, 'HIGH');
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (15, 'IBLD11', 15, 1, 'MID', 14);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (16, 'IBLD111', 14, 1, 'LOW', 15);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (17, 'IBLD12', 16, 1, 'MID', 14);
+INSERT INTO oe_code (id, name, total_employees, department_id, type, parent_oe_code_id) VALUES (18, 'IBLD121', 15, 1, 'LOW', 17);
 
 INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (1, 'M12345', 'User-11', 'ADMIN', 1, 1);
 INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (2, 'M12346', 'User-1', 'HIGH_LEVEL_MANAGER', 1, 1);
@@ -115,21 +115,73 @@ INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (3
 INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (37, 'M12381', 'User-1236', 'EMPLOYEE', 1, 13);
 INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (38, 'M12382', 'User-1237', 'EMPLOYEE', 1, 13);
 
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (39, 'M12383', 'User-2', 'HIGH_LEVEL_MANAGER', 1, 14);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (40, 'M12384', 'User-21', 'LOW_LEVEL_MANAGER', 1, 15);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (41, 'M12385', 'User-211', 'EMPLOYEE', 1, 16);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (42, 'M12386', 'User-212', 'EMPLOYEE', 1, 16);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (43, 'M12387', 'User-213', 'EMPLOYEE', 1, 16);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (44, 'M12388', 'User-214', 'EMPLOYEE', 1, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (39, 'M12383', 'User-2', 'HIGH_LEVEL_MANAGER', 2, 14);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (40, 'M12384', 'User-21', 'LOW_LEVEL_MANAGER', 2, 15);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (41, 'M12385', 'User-211', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (42, 'M12386', 'User-212', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (43, 'M12387', 'User-213', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (44, 'M12388', 'User-214', 'EMPLOYEE', 2, 16);
 
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (45, 'M12389', 'User-22', 'LOW_LEVEL_MANAGER', 1, 17);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (46, 'M12390', 'User-221', 'EMPLOYEE', 1, 18);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (47, 'M12391', 'User-222', 'EMPLOYEE', 1, 18);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (48, 'M12392', 'User-223', 'EMPLOYEE', 1, 18);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (49, 'M12393', 'User-224', 'EMPLOYEE', 1, 18);
-INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (50, 'M12394', 'User-225', 'EMPLOYEE', 1, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (45, 'M12389', 'User-22', 'LOW_LEVEL_MANAGER', 2, 17);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (46, 'M12390', 'User-221', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (47, 'M12391', 'User-222', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (48, 'M12392', 'User-223', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (49, 'M12393', 'User-224', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (50, 'M12394', 'User-225', 'EMPLOYEE', 2, 18);
 
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (51, 'M12395', 'User-1116', 'EMPLOYEE', 1, 4);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (52, 'M12396', 'User-1117', 'EMPLOYEE', 1, 4);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (53, 'M12397', 'User-1118', 'EMPLOYEE', 1, 4);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (54, 'M12398', 'User-1119', 'EMPLOYEE', 1, 4);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (55, 'M12399', 'User-11191', 'EMPLOYEE', 1, 4);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (56, 'M12400', 'User-11192', 'EMPLOYEE', 1, 4);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (57, 'M12401', 'User-11193', 'EMPLOYEE', 1, 4);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (58, 'M12402', 'User-11194', 'EMPLOYEE', 1, 4);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (59, 'M12403', 'User-11195', 'EMPLOYEE', 1, 4);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (60, 'M12404', 'User-11196', 'EMPLOYEE', 1, 4);
 
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (61, 'M12405', 'User-1126', 'EMPLOYEE', 1, 6);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (62, 'M12406', 'User-1127', 'EMPLOYEE', 1, 6);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (63, 'M12407', 'User-1128', 'EMPLOYEE', 1, 6);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (64, 'M12408', 'User-1129', 'EMPLOYEE', 1, 6);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (65, 'M12409', 'User-11291', 'EMPLOYEE', 1, 6);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (66, 'M12410', 'User-11292', 'EMPLOYEE', 1, 6);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (67, 'M12411', 'User-11293', 'EMPLOYEE', 1, 6);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (68, 'M12412', 'User-11293', 'EMPLOYEE', 1, 6);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (69, 'M12413', 'User-11295', 'EMPLOYEE', 1, 6);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (70, 'M12414', 'User-11296', 'EMPLOYEE', 1, 6);
+
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (71, 'M12415', 'User-1226', 'EMPLOYEE', 1, 11);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (72, 'M12416', 'User-1227', 'EMPLOYEE', 1, 11);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (73, 'M12417', 'User-1228', 'EMPLOYEE', 1, 11);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (74, 'M12418', 'User-1229', 'EMPLOYEE', 1, 11);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (75, 'M12419', 'User-12291', 'EMPLOYEE', 1, 11);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (76, 'M12420', 'User-12292', 'EMPLOYEE', 1, 11);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (77, 'M12421', 'User-12293', 'EMPLOYEE', 1, 11);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (78, 'M12422', 'User-12294', 'EMPLOYEE', 1, 11);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (79, 'M12423', 'User-12295', 'EMPLOYEE', 1, 11);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (80, 'M12424', 'User-12296', 'EMPLOYEE', 1, 11);
+
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (81, 'M12425', 'User-226', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (82, 'M12426', 'User-227', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (83, 'M12427', 'User-228', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (84, 'M12428', 'User-229', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (85, 'M12429', 'User-2291', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (86, 'M12430', 'User-2292', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (87, 'M12431', 'User-2293', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (88, 'M12432', 'User-2294', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (89, 'M12433', 'User-2295', 'EMPLOYEE', 2, 18);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (90, 'M12434', 'User-2296', 'EMPLOYEE', 2, 18);
+
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (91, 'M12435', 'User-216', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (92, 'M12436', 'User-217', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (93, 'M12437', 'User-218', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (95, 'M12438', 'User-219', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (96, 'M12439', 'User-2191', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (97, 'M12440', 'User-2192', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (98, 'M12441', 'User-2193', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (99, 'M12442', 'User-2194', 'EMPLOYEE', 2, 16);
+INSERT INTO employee (id, mpid, name, role, department_id, oe_code_id) VALUES (100, 'M12443', 'User-2195', 'EMPLOYEE', 2, 16);
 
 
 INSERT INTO department_admin (id, employee_id, department_id) VALUES (1, 1, 1);
