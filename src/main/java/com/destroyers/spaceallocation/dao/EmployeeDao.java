ee035755b7a03579aea18e519c13f9f59b8fa7b3
Shilkumar Jadhav <shilkumarjadhav@gmail.com>
2022-09-18 11:34:38 +0530

    Added query param for OE code to get employees by OE code

--- src/main/java/com/destroyers/spaceallocation/dao/EmployeeDao.java
+++ src/main/java/com/destroyers/spaceallocation/dao/EmployeeDao.java
@@ -2,11 +2,12 @@ package com.destroyers.spaceallocation.dao;
 
 import com.destroyers.spaceallocation.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface EmployeeDao extends JpaRepository<Employee, Long> {
+public interface EmployeeDao extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
     Optional<Employee> findByMpid(String mpid);
 }
